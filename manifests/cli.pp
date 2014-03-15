027cb1681ba634e17dadf8fd13672dd1d0221e6a
zsim0n <zoltan@zoltansimon.me>
2014-03-16 00:00:36 +0100

    add git-command

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -1,6 +1,6 @@
 class wordpress::cli {
   file { '/usr/share/wp-cli':
-    ensure  => "directory",
+    ensure  => 'directory',
   }
 
   exec { 'wp-cli_install' :
@@ -14,5 +14,10 @@ class wordpress::cli {
     target  => '/usr/share/wp-cli/bin/wp',
     force   => true,
   }
-  File['/usr/share/wp-cli']->Exec['wp-cli_install']->File['wp-cli_link']
+  
+  exec 'add-wp-cli-git-command' :
+    command => 'sudo composer config repositories.wp-cli composer http://wp-cli.org/package-index/ && sudo composer require mattes/wp-cli-git-command=dev-master',
+    cwd     => '/usr/share/wp-cli',
+  }
+  File['/usr/share/wp-cli']->Exec['wp-cli_install']->File['wp-cli_link']->Exec['add-wp-cli-git-command']
 }
